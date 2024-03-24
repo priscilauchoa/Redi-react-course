fe79b495c54eda1b3a5c752df7ec4c9d8d5d285d
Priscila Flores <priscilauchoaa@gmail.com>
2024-03-24 19:45:04 +0100

    remove unecessary line

--- first-homework/src/App.jsx
+++ first-homework/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
     <>
        <div>
           <a href="https://github.com/priscilauchoa"> <img src="https://avatars.githubusercontent.com/u/22195904?v=4" className="logo" alt="Vite logo" /></a>
-        
       </div>
       <h1>Priscila Flores </h1>
       <div className="card">
