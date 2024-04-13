0ee2616918f6f4d4eae32c7a64a8e9a04e158f83
Priscila Flores <priscilauchoaa@gmail.com>
2024-04-13 12:51:11 +0200

    extract counter components

---
+++ homework/src/components/Counter/Counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  function minus() {
+    count < 1 ? count : setCount(count - 1);
+  }
+  function plus() {
+    setCount(count + 1);
+  }
+  return (
+    <div className="container">
+      <h2>Counter exercise class 7</h2>
+      <div className="counter">
+        <CounterButton onClick={minus} label="-" bc="btn-red" />
+        <CounterContent count={count} />
+        <CounterButton onClick={plus} label="+" bc="btn-green" />
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
+
+function CounterButton({ onClick, label, bc }) {
+  return (
+    <button className={bc} onClick={onClick}>
+      {label}
+    </button>
+  );
+}
+
+function CounterContent({ count }) {
+  return <div className="counter">{count}</div>;
+}
