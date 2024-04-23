6a999198c4436fd254bd73a0bb14aa47b95cb506
Priscila Flores <priscilauchoaa@gmail.com>
2024-04-23 16:44:03 +0200

    add components to app

--- homework/src/App.jsx
+++ homework/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import Counter from './components/Counter/Counter.jsx'
-import CharacterCounter from '../src/components/CharacterCounter/CharacterCounter'
-import RestaurantView from '../src/components/RestaurantView/RestaurantView.jsx'
-import PlayList from './components/PlayList/PlayList'
-import Lesson8 from './components/Lesson8/Lesson8.jsx'
-import Modal from './components/Modal/Modal.jsx'
+import Counter from "./components/Counter/Counter.jsx";
+import CharacterCounter from "../src/components/CharacterCounter/CharacterCounter";
+import RestaurantView2 from "../src/components/RestaurantView2/RestaurantView2.jsx";
+import PlayList from "./components/PlayList/PlayList";
+import Lesson8 from "./components/Lesson8/Lesson8.jsx";
+import Modal from "./components/Modal/Modal.jsx";
 import Profile from "./components/Profile/Profile.jsx";
+// import RestaurantView from "./components/views/RestaurantView.jsx";
 /* eslint-disable react/prop-types */
 
 function App() {
@@ -13,12 +14,12 @@ function App() {
     <>
       <div>
         <h2>Week 1 - First React Component</h2>
-        <Profile message="First Week"/>
+        <Profile message="First Week" />
         <h2>Week 2 - Props and State</h2>
         <CharacterCounter />
         <h2> Week 3 - Props, State, and events</h2>
         <Counter />
-        <RestaurantView />
+        {/* <RestaurantView /> */}
         <h2> Week 4 - Props, State, and Components Practice </h2>
         {/* <PlayList /> started the exercise here and add more functionalities on the week 5 */}
         <Lesson8 />
@@ -26,9 +27,16 @@ function App() {
         <h3>Personal Practice</h3>
         <Modal />
         <h2>Week 6 - Conditional rendering </h2>
-        <PlayList />
+        {/* <PlayList /> started the exercise here and add more functionalities on the week 8 */}
+        <RestaurantView2 />
+        {/* <RestaurantView /> */}
         <h2>Week 7</h2>
-
+        <h2>
+          Organize folders and apply Css Modules to Playlist and ButtonModule
+          components
+        </h2>
+        <h2>Week 8</h2>
+        <PlayList />
       </div>
     </>
   );
